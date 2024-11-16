0981d75af53ee3ab18fb5cd12a6dec314939c01b
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-11-16 00:33:01 -0500

    change Login to Enter

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,30 +1,28 @@
 import { Link } from "react-router-dom";
-import "./Login.css"
-import wifi from '../../assets/wifi.png';
-
+import "./Login.css";
+import wifi from "../../assets/wifi.png";
 
 export default function Login() {
-    
-    return (
-        <div className="login">
-            <div className="circle">
-                <div className="circle1">
-                    <div className="circle2">
-                        <div className="circle3">
-                        <img src={wifi} alt="google icon" />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="login">
+      <div className="circle">
+        <div className="circle1">
+          <div className="circle2">
+            <div className="circle3">
+              <img src={wifi} alt="google icon" />
             </div>
-            <span>LinkNYC Finder</span>
-            <div className="login-button__container">
-                <div className="edition">
-                <p>2024 EDITION</p>
-                </div>
-                <Link to="/home">
-                    <button className="login-button">LOGIN</button>
-                </Link>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+      <span>LinkNYC Finder</span>
+      <div className="login-button__container">
+        <div className="edition">
+          <p>2024 EDITION</p>
+        </div>
+        <Link to="/home">
+          <button className="login-button">ENTER</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
